3b4179fefa80f6add8ac2fd847613999de58e830
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2025-05-03 20:08:28 +0300

    feat: set up the project and bot key

---
+++ Database/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TelegramStatsBot.Database
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
+using Telegram.Bot;
+using Microsoft.EntityFrameworkCore;
+using TelegramStatsBot.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var botToken = builder.Configuration["BOT_TOKEN"];
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add services to the container.
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(botToken));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,8 +28,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
